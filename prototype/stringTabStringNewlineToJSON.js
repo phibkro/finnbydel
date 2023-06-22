8a46e630483a1b5316a39ddd9bf15ed746875366
Skuiggly <piplup107@gmail.com>
2023-06-22 20:06:49 +0200

    fix: remove spaces

--- prototype/stringTabStringNewlineToJSON.js
+++ prototype/stringTabStringNewlineToJSON.js
@@ -1,4 +1,4 @@
-import { oslo, bergen } from "./rawBydelnrData.js";
+import { oslo, bergen, trondheim, stavanger } from "./rawBydelnrData.js";
 function stringTabStringNewlineToJSON(text) {
   let jsonObject = '{"';
   for (let i = 0; i < text.length; i++) {
@@ -8,6 +8,9 @@ function stringTabStringNewlineToJSON(text) {
     }
     if (text[i] === "\n") {
       jsonObject += '","';
+      continue;
+    }
+    if (text[i] === " ") {
       continue;
     }
     jsonObject += text[i];
