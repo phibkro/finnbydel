d8eee100b247432e3fb8d99be194939f6eff8b56
phibkro <philib.krogh@gmail.com>
2023-07-17 20:00:34 +0200

    remove lib

--- lib/createAdresseBydelData.ts
+++
@@ -1,9 +0,0 @@
-import { composeDictionaryFromMulticolumnTSV } from "./composeDictionary";
-
-const destination = "../data/adresseBydel.ts";
-const text = await Deno.readTextFile("../data/postnr_adresse_bydel.tsv");
-await Deno.writeTextFile(
-  destination,
-  "export const adresseBydel = " + composeDictionaryFromMulticolumnTSV(text, 3)
-);
-console.log(`File written to ${destination}`);
