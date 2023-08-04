a39f80e9f477b27d40b2b6fb8f390b4306a255c9
phibkro <philib.krogh@gmail.com>
2023-08-04 23:03:05 +0200

    fix blank homepage

--- finnbydel-app/src/pages/index.tsx
+++ finnbydel-app/src/pages/index.tsx
@@ -1,3 +1,24 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <></>;
+  const cities = ["Bergen", "Oslo", "Trondheim", "Stavanger"];
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center gap-4  bg-slate-900 text-white">
+        <h1 className="text-5xl">Finn bydelen din</h1>
+        <p className="text-center text-3xl ">
+          Usikker på hvilken bydel du bor i? Lurer du på hvilken bydel en
+          adresse tilhører? Søk på adresser her.
+        </p>
+        <p className="text-3xl">Hvilken by ligger adressen i?</p>
+        <ul>
+          {cities.map((name, id) => (
+            <li key={id} className="text-3xl">
+              <Link href={name}>{name}</Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
 }
