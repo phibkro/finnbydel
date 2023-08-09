11c8fac1d139d8d962954dda72d97e140e5ba009
phibkro <philib.krogh@gmail.com>
2023-08-09 16:55:54 +0200

    update tailwind colors

--- finnbydel-app/tailwind.config.ts
+++ finnbydel-app/tailwind.config.ts
@@ -3,7 +3,20 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // oslo.kommune.no farger
+        "blue-light": "#b3f5ff",
+        "blue-dark": "#1f42aa",
+        "blue-darkdark": "00171a",
+        "purple-dark": "#2a2859",
+        "gray-light": "#f9f9f9",
+        "gray-dark": "#212121",
+        "gray-darkdark": "#1d1d1d",
+        "yellow-medium": "#f9c66b",
+        "black-kinda": "#0E0E0E",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
