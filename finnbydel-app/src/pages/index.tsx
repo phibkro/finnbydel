e67db753d2f3cd97157b0d85b839909d4c855e57
phibkro <philib.krogh@gmail.com>
2023-08-09 17:16:13 +0200

    refactor into layout for styling index page

--- finnbydel-app/src/pages/index.tsx
+++ finnbydel-app/src/pages/index.tsx
@@ -33,21 +33,15 @@ export default function Home() {
   }
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center gap-4  bg-slate-900 text-white">
-        <h1 className="text-5xl">Finn bydelen din</h1>
-        <p className="text-center text-3xl ">
-          Usikker på hvilken bydel du bor i? Lurer du på hvilken bydel en
-          adresse tilhører? Søk på adresser her.
-        </p>
-        <p className="text-3xl">Hvilken by ligger adressen i?</p>
-        <ul>
-          {cityQuery.data.map((result) => (
-            <li key={result.id} className="text-3xl">
-              <Link href={result.name}>{result.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </main>
+      <ul>
+        {cityQuery.data.map((result) => (
+          <li key={result.id} className={`text-3xl`}>
+            <Link href={result.name} className="hover:text-blue-dark">
+              {result.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
